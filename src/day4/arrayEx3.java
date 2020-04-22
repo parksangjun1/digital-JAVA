433111a2150243f775a18789e4a31d5f5cce952b
parksangjun <psjpsj4848@naver.com>
2020-04-22 16:59:23 +0900

    4일차 향상된for문 예제

---
+++ src/day4/arrayEx3.java
@@ -0,0 +1,20 @@
+package day4;
+
+public class arrayEx3 {
+
+	public static void main(String[] args) {
+		// 향상된 for문 예제
+		int [] arr = new int[] {1,3,5,7,2,4,6,8};
+		/*향상된 for문을 주로 사용하는경우
+		 * 배열의 모든값을 탐색하는경우
+		 * 배열의 값을 수정하지 않는 경우
+		 * 
+		 */
+		
+		for(int tmp : arr) {
+			System.out.print(tmp+" ");
+		}
+
+	}
+
+}
