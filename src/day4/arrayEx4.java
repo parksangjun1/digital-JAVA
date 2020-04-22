4c5a7d75aea124001ae3e1ffcf14d6919a53dfa9
parksangjun <psjpsj4848@naver.com>
2020-04-22 17:00:15 +0900

    4일차 배열에 임의의 수 넣는 예제

---
+++ src/day4/arrayEx4.java
@@ -0,0 +1,50 @@
+package day4;
+
+public class arrayEx4 {
+
+	public static void main(String[] args) {
+		// 배열에 임의의 수 넣는 예제
+		int arr[] = new int [7];
+		int max = 45;
+		int min = 1;
+		//중복가능한 임의의 수를 생성하여 배열에 저장(중복가능)
+		for(int i = 0; i < arr.length; i++ ) {
+			int random = (int)(Math.random()*(max-min+1)) + min;
+			arr[i] = random;		
+		}
+		for(int tmp:arr) {
+			System.out.print(tmp + " ");
+		}
+		//임의의 수를 생성하여 배열에 저장 (중복 안됨)
+		
+		int cnt = 0; //배열에 저장된 랜덤수의 개수 
+		int []arr2 = new int [7];
+		while(cnt < arr2.length/*배열에 저장된 랜덤수의 갯수가 3보다 작을때*/ ) { 
+			/*생성된 랜덤수 random : 1 1 2 9
+			 * 배열에 저장된 갯수 cnt : 1 ++
+			 * 저장된 배열의 값 arr : 1 0 0
+			 */
+		
+			int random = (int)(Math.random()*(max-min+1)) + min;
+			int i = 0;
+			while(i<cnt) {
+				if(random == arr2[i]) {
+					break;
+				}
+				i++;
+			}
+			if(i == cnt) {				  //배열의 0번지부터 (배열에 저장된 랜덤수)'cnt'의 갯수-1번지까지 중복여부 체크하여
+				                           //중복되지 않으면 배열에 랜덤수를 저장하고 배열에 저장된 랜덤수의 갯수를 1증가
+					arr2[cnt]= random;
+					cnt++;
+			
+			}
+			
+		}
+				System.out.println();
+				for(int tmp:arr2) {
+					System.out.print(tmp + " ");
+				}
+
+	}
+}
