dbda06d6b99507118a5c60eb4e46edc82f6c50aa
parksangjun <psjpsj4848@naver.com>
2020-05-18 17:15:20 +0900

    18일차 입력받은정수 거꾸로출력(list)

---
+++ src/day18/TestEx9.java
@@ -0,0 +1,29 @@
+package day18;
+
+import java.util.*;
+
+
+public class TestEx9 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("입력받을 정수의 개수 : ");
+		int size = scan.nextInt();
+		//int arr[] = new int[size];
+		List<Integer> arr = new ArrayList<Integer>();
+		System.out.print("정수"+  size + "개를 입력하세요.");
+		for(int i = 0; i < size; i++) {			
+			int tmp = scan.nextInt();
+			//arr[i] = tmp;
+			arr.add(tmp);
+			//arr.add(scan.nextInt);
+			
+		}
+		System.out.println("입력 받은 정수를 거꾸로 출력");
+		for(int i = size-1; i>= 0; i--) {
+			//System.out.print(arr[i] + " ");
+			System.out.print(arr.get(i) + " ");
+		}
+	}	
+}
