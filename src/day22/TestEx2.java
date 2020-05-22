2fd3afe79423944c4475c8f620c7abce5280438d
parksangjun <psjpsj4848@naver.com>
2020-05-22 17:45:23 +0900

    22일차 배열이용한 예제

---
+++ src/day22/TestEx2.java
@@ -0,0 +1,39 @@
+package day22;
+
+import java.util.*;
+
+public class TestEx2 {
+
+	public static void main(String[] args) {
+		/* 배열에서 가장 큰 값을 출력하는 예제를 이용하여
+		 * 배열에서 가장 큰 값을 반환하는 메소드를 생성하고, main 메소드에서 테스트해보세요.
+		 */
+		/*Scanner scan = new Scanner(System.in);		
+		int arr[] = new int[5];
+		System.out.println("정수를 입력하세요.");
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();			
+		}
+		
+		System.out.println(Max(arr, scan));
+
+	}
+	public static int Max(int arr[], Scanner scan) {		
+
+		int max = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}*/
+
+	}
+}
+	/* 기능 : 주어진 배열의 합을 구하는 메소드
+	 * 매개변수 : 배열, 주어진 수 => arr[], int num
+	 * 리턴타입 : 합 : int
+	 * 메소드명 : sum
+	 */
+
